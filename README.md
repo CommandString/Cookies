ae961e47e2cc7cae393ba7e277a71195f6dd8113
root <root@CMDSTRPC>
2022-08-21 04:53:06 -0400

    v1.0.0

---
+++ README.md
@@ -0,0 +1,124 @@
+# [cmdstr/cookies](https://packagist.org/packages/cmdstr/cookies) - A simpler way to manipulate cookies in PHP #
+
+### Install with Composer using `composer require cmdstr/cookies` ###
+
+## Requirements ##
+- PHP >=8.0
+- Basic understanding of PHP OOP
+- Basic understanding of MySQL
+- Composer 2
+
+## Basic Usage ##
+```php
+require  __DIR__."/vendor/autoload.php";
+use cmdstr\cookies\cookie;
+
+#                       v 31 character string              v Encryption method #
+$cookies = new cookies("MZCdg02STLzrsj05KE3SIL62SSlh2Ij", "AES-256-CTR");
+
+#                              v hours 
+#             v name  v value  v valid v seconds valid
+$cookies->set("name", "value", 168, 10, 30); // by default cookies expire in a week
+#                               ^ minutes valid
+
+// After page refresh (An exception will be thrown if the cookie doesn't exist) //
+echo $cookies->get("name"); // output: value
+
+// Delete cookie (An exception will be thrown if the cookie doesn't exist) //
+$cookie->delete("name"); // remove the cookie
+
+// Delete all cookies
+$cookie->deleteAll();
+
+// Check if a cookie exists
+$cookie->exists("name"); // returns bool
+```
+
+## Comparing regular cookie manipulation with cmdstr/cookies ##
+### cmdstr/cookies ###
+```php
+// config.php
+require  __DIR__."/vendor/autoload.php";
+use cmdstr\cookies\cookie;
+
+$cookies = new cookies("MZCdg02STLzrsj05KE3SIL62SSlh2Ij", "AES-256-CTR");
+
+// ...
+
+// login.php
+require_once "config.php";
+
+if ($userIsReadyToBeLoggedIn) {
+	$cookies->set("username", "Command_String");
+	header("location: home.php");
+}
+// ...
+
+// home.php
+<?php
+require_once "config.php";
+
+if (!$cookies->exists("username")) {
+	header("location: login.php");
+}
+```
+```html
+// ...
+<h1>Welcome back, <?= $cookies->get("username")); ?></h1>
+// ...
+```
+```php
+// logout.php
+require_once "config.php";
+
+$cookies->deleteAll();
+header("location: login.php");
+```
+### Regular Cookie Manipulation ###
+```php
+// config.php
+$cookies = [
+	"passphrase" => "MZCdg02STLzrsj05KE3SIL62SSlh2Ij",
+	"method" => "AES-256-CTR"
+];
+
+// ...
+
+// login.php
+require_once "config.php";
+
+if ($userIsAbleToBeLoggedIn){
+	$characters = str_split("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ0123456789");
+	$iv = "";
+
+	for($i=0; $i < 16; $i++) {
+		$iv .= $characters[rand(0, count($characters)-1)];
+	}
+	$encryptedString = openssl_encrypt("Command_String", $cookies['method'], $cookies['passphrase'], 0, $iv);
+	
+	setcookie($name, "$iv:$encryptedString", time() + (3600 * 168), "/");
+	header("location: home.php");
+}
+
+// home.php
+require_once "config.php";
+
+if (isset($_COOKIE['username'])) {
+	header("location: login.php");
+}
+
+// ...
+```
+```html
+<h1>Welcome back, <?= $_COOKIE['username'] ?><h1>
+<!-- ... -->
+```
+```php
+// logout.php
+foreach ($_COOKIE as $key => $value) {
+	unset($_COOKIE[$key]);
+	setcookie($key, null, -1, '/');
+}
+
+header("location: login.php");
+```
---
+++ src/cookie.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace cmdstr\cookies;
+
+use Exception;
+use InvalidArgumentException;
+
+/**
+ * An simpler way to manipulate cookies in PHP
+ * 
+ * @author Command_String - https://discord.dog/232224992908017664
+ */
+class cookie {
+    /**
+     * Passphrase key - must be a 31 character alphanumeric string
+     * 
+     * @property string $passphrase
+     */
+    private string $passphrase;
+
+    /**
+     * Encryption method
+     * 
+     * @property string $encryptionMethod
+     */
+    private string $encryptionMethod;
+
+    /**
+     * @param string $passphrase
+     */
+    public function __construct(string $passphrase, string $encryptionMethod) {
+        if (strlen($passphrase) !== 31) {
+            throw new InvalidArgumentException("Passphrase must be 31 character string");
+        }
+
+        if (!in_array($encryptionMethod, openssl_get_cipher_methods(true))) {
+            throw new InvalidArgumentException("Encryption method doesn't exist. Use var_dump(openssl_get_cipher_methods(true)); to view all available methods.");
+        }
+
+        $this->passphrase = $passphrase;
+
+        $this->encryptionMethod = $encryptionMethod;
+    }
+
+    /**
+     * Generates a 16 character alphanumeric string
+     * 
+     * @return string
+     */
+    private function generateIv():string
+    {
+        $characters = str_split("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ0123456789");
+        $iv = "";
+
+        for($i=0; $i < 16; $i++) {
+            $iv .= $characters[rand(0, count($characters)-1)];
+        }
+        
+        return $iv;
+    }
+
+    /**
+     * Decrypt data
+     * 
+     * @param string $data
+     * @return string
+     */
+    private function decrypt(string $data):string
+    {
+        $parts = explode(":", $data);
+
+        $iv = $parts[0];
+        $encryptedString = $parts[1];
+
+        return openssl_decrypt($encryptedString, $this->encryptionMethod, $this->passphrase, 0, $iv);
+    }
+
+    /**
+     * Set cookie
+     * 
+     * @param string $name
+     * @param string $value
+     * @param int $hoursValid
+     */
+    public function set(string $name, string $value, int $hoursValid=168, int $minutesValid=0, int $secondsValid=0):cookie
+    {
+        $iv = $this->generateIv();
+
+        $encryptedString = openssl_encrypt($value, $this->encryptionMethod, $this->passphrase, 0, $iv);
+
+        setcookie($name, "$iv:$encryptedString", time() + ((3600 * $hoursValid) + (60 * $minutesValid) + $secondsValid), "/");
+
+        return $this;
+    }
+
+    public function exists(string $name):bool
+    {
+        return isset($_COOKIE[$name]);
+    }
+
+    /**
+     * Get cookie
+     * 
+     * @param string $name
+     * @return string
+     */
+    public function get(string $name):string
+    {
+        if ($this->exists($name)) {
+            return $this->decrypt($_COOKIE[$name]);
+        } else {
+            throw new \InvalidArgumentException("Cookie doesn't exist in configuration");
+        }
+    }
+
+    /**
+     * Delete cookie(s) specified
+     * 
+     * @param string $name
+     */
+    public function delete(string ...$cookies):cookie
+    {
+        foreach ($cookies as $cookie) {
+            if ($this->exists($_COOKIE[$cookie])) {
+                throw new \InvalidArgumentException("Cookie doesn't exist in configuration");
+            }
+
+            unset($_COOKIE[$cookie]);
+            setcookie($cookie, null, -1, '/');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Deletes all cookies
+     * 
+     * @return void
+     */
+    public function deleteAll():cookie 
+    {
+        foreach ($_COOKIE as $key => $value) {
+            $cookies[] = $key;
+        }
+
+        $this->delete($cookies);
+        
+        return $this;
+    }
+}
